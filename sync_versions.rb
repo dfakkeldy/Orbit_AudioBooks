02e525c79b2a14b0c4430d6e810bc9bd69bbb016
Dan Fakkeldy <2112591+dfakkeldy@users.noreply.github.com>
2026-05-03 23:22:56 -0300

    Bump version to 1.0.1 (build 5)

--- sync_versions.rb
+++ sync_versions.rb
@@ -5,10 +5,10 @@ project = Xcodeproj::Project.open(project_path)
 
 project.targets.each do |target|
   target.build_configurations.each do |config|
-    config.build_settings['CURRENT_PROJECT_VERSION'] = '4'
-    config.build_settings['MARKETING_VERSION'] = '1.0'
+    config.build_settings['CURRENT_PROJECT_VERSION'] = '5'
+    config.build_settings['MARKETING_VERSION'] = '1.0.1'
   end
 end
 
 project.save
-puts "Project versions synced to 4"
+puts "Project versions synced to 5 / 1.0.1"
